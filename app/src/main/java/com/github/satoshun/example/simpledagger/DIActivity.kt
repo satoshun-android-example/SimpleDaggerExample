608c7d1fb68cb7ce56a1799a0a632f7f4bcaae4b
SatoShun <shun.sato1@gmail.com>
2018-09-05 06:59:31 +0000

    Fragment ext

--- app/src/main/java/com/github/satoshun/example/simpledagger/DIActivity.kt
+++ app/src/main/java/com/github/satoshun/example/simpledagger/DIActivity.kt
@@ -1,9 +1,16 @@
 package com.github.satoshun.example.simpledagger
 
 import android.app.Activity
+import androidx.fragment.app.Fragment
 
 inline fun <reified T> Activity.get(): T {
   return (application as App).get()
 }
 
 inline fun <reified T> Activity.inject() = lazy { get<T>() }
+
+inline fun <reified T> Fragment.get(): T {
+  return (requireActivity().application as App).get()
+}
+
+inline fun <reified T> Fragment.inject() = lazy { get<T>() }
